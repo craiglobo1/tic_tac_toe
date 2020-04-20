1a811a2e44fd971a190bd32344abf3140257cac3
Craig lobo <craiglobo1@gmail.com>
2020-04-20 20:42:08 +0400

    Finished

--- tictactoe.py
+++ tictactoe.py
@@ -4,6 +4,7 @@ Tic Tac Toe Player
 
 import math
 import copy
+from random import randint
 
 X = "X"
 O = "O"
@@ -67,42 +68,129 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    for row in board:
+    XdiagDown = XdiagUp = OdiagDown = OdiagUp = 0
+    for i,row in enumerate(board):
         if row.count(X) == 3:
             return X
         if row.count(O) == 3:
             return O
-    for i in range(3):
-        if board[i][0] == X or board[i][1] == X or board[i][2] == X:
+
+        if board[0][i] == X and board[1][i] == X and board[2][i] == X:
             return X
-        if board[i][0] == O or board[i][1] == O or board[i][2] == O:
+        if board[0][i] == O and board[1][i] == O and board[2][i] == O:
             return O
-    
+
+        if board[i][i] == X:
+            XdiagDown += 1
+        if board[i][2-i] == X:
+            XdiagUp += 1
+        if board[i][i] == O:
+            OdiagDown += 1
+        if board[i][2-i] == O:
+            OdiagUp += 1
+    if 3 in [XdiagDown,XdiagUp]:
+        return X
+    if 3 in [OdiagDown,OdiagUp]:
+        return O
+    return None
 
 
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    raise NotImplementedError
+    if winner(board) != None:
+        return True
+    if any( None in row for row in board):
+        return False
+    else:
+        return True
 
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    winnerOfRound = winner(board)
+    if winnerOfRound == X:
+        return 1
+    elif winnerOfRound == O:
+        return -1
+    else:
+        return 0
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    board = copy.deepcopy(board)
+
+    BestScore = -math.inf
+    player_name = player(board)
+
+    if terminal(board):return None
+
+    if board == initial_state():
+        i, j = randint(0, 2), randint(0, 2)
+        return (i, j)
+        
+    
+
+
+
+    if player_name == X:
+        value = -math.inf
+        move = (-1, -1)
+        for action in actions(board):
+            minv = MIN_VALUE(result(board, action))
+            if minv == 1:
+                move = action
+                break
+            if minv > value:
+                value = minv
+                move = action
+        return move
+
+    if player_name == O:
+        value = math.inf
+        move = (-1, -1)
+        for action in actions(board):
+            maxv = MAX_VALUE(result(board, action))
+            if maxv == -1:
+                move = action
+                break
+            if maxv < value:
+                value = maxv
+                move = action
+        return move
+
+        
+
+
+def MAX_VALUE(board):
+    if terminal(board):
+        return utility(board)
+    v = -math.inf
+    for action in actions(board):
+        v = max(v, MIN_VALUE(result(board, action)))
+        if v == 1:
+            break
+    return v
+
+def MIN_VALUE(board):
+    if terminal(board):
+        return utility(board)
+    v = math.inf
+    for action in actions(board):
+        v = min(v, MAX_VALUE(result(board, action)))
+        if v == -1:
+            break
+    return v
 
 # print(actions([[EMPTY, X, EMPTY],
 #             [X, EMPTY, O],
 #             [EMPTY, O, EMPTY]]))
-# print(result([[EMPTY, EMPTY, EMPTY],
-#             [EMPTY, EMPTY, EMPTY],
-#             [EMPTY, EMPTY, EMPTY]],(2,2)))
+# print(winner([[O, O, O],
+#             [EMPTY, O, X],
+#             [X, EMPTY, X]]))
